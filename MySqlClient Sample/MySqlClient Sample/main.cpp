ebc9e46d00b6c5583a8105234f1b65cefd9aa5c1
LyMast <lymast3578@naver.com>
2024-04-17 07:04:13 +0900

    Add Samples

---
+++ MySqlClient Sample/MySqlClient Sample/main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int wmain(int argc, WCHAR* argv[])
+{
+
+
+
+
+	return 0;
+}
---
+++ VMQuery Sample/VMQuery Sample/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int wmain(int argc, WCHAR* argv[])
+{
+
+
+
+
+	return 0;
+}
